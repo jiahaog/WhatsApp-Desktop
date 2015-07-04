f4c990e9b9e5fc2559b1044e50f8dd699586f44a
Jia Hao <jiahaog@gmail.com>
2015-07-04 12:20:56 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# WhatsApp Desktop
+
+## Introduction
+
+An experiment with [Electron](http://electron.atom.io), in an attempt to create a simple WhatsApp desktop client with the sole purpose of being able to `⌘-tab` or `alt-tab` to.
+
+## Installation
+
+### Install electron
+```bash
+$ npm install electron-prebuilt -g
+```
+### Clone the repo
+
+### Run it
+```bash
+$ cd <Repo root>
+$ electron .
+```
+
+## To-Do
+
+- Desktop Notifications
+- Save application state
+- Everything else
---
+++ main.js
@@ -0,0 +1,38 @@
+/**
+ * Created by JiaHao on 4/7/15.
+ */
+
+
+var app = require('app');
+
+var BrowserWindow = require('browser-window');
+
+require('crash-reporter').start();
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow(
+        {
+            width: 800,
+            height: 600,
+            'web-preferences': {
+                javascript: true,
+                plugins: true,
+            }
+        }
+    );
+
+    mainWindow.loadUrl('file://' + __dirname + '/index.html');
+    //mainWindow.openDevTools();
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    })
+});
+
